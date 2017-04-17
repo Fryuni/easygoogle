9e1b25231694a3d44fb4653e371b534aa9b00731
Luiz Augusto Ferraz <adm.fryuni@gmail.com>
2017-04-16 23:08:48 -0300

    Changed distutils for setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
         name = "easygoogle",
         packages = ["easygoogle"],
         license = "https://github.com/Fryuni/easy_google/blob/master/LICENSE",
-        version = '0.1beta1',
+        version = '0.1b1',
         description = "Easy to use wrapper to google APIs",
         author = "Luiz Augusto Ferraz",
         author_email = "adm.fryuni@gmail.com",
-        requires = ["googleapipythonclient (>=1.6.2)"],
-        url = "https://github.com/Fryuni/easy_google/archive/0.1beta1.tar.gz",
+        install_requires = ["google-api-python-client (>=1.6.2)",
+            "setuptools"],
+        url = "https://github.com/Fryuni/easy_google/archive/0.1b1.tar.gz",
         keywords = ['google', 'apis', 'google-apis'],
         classifiers = []
         )
