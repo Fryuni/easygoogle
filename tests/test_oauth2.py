c2072f0ad421fd4c424ec4abe635a12c450d7ce5
Luiz Augusto Ferraz <l@mestra.org>
2017-11-05 08:58:41 -0200

    Tests for oauth2 authentication

---
+++ tests/test_oauth2.py
@@ -0,0 +1,54 @@
+import json
+import os
+
+import easygoogle
+
+__path__ = os.path.dirname(os.path.abspath(__file__))
+
+MOCKED_APIS = json.load(open(os.path.join(__path__, 'data', 'apis.json')))
+
+SCOPES = ['scope.unique', 'scope.multiple']
+RESULT_SCOPES = ["https://testscopes.exemple.org/auth/scope.multiple",
+                 "https://testscopes.exemple.org/auth/scope.unique"]
+
+
+def test_inheritance():
+    assert issubclass(easygoogle.oauth2, easygoogle._api_builder)
+
+
+def test_creation_call_server(mocker, tmpdir):
+    mocker.patch.dict('easygoogle.apisDict', values=MOCKED_APIS)
+    mocker.patch('easygoogle.InstalledAppFlow')
+    mocker.patch('easygoogle.google')
+    mocker.patch('easygoogle.os.path.isfile', return_value=False)
+    mocker.patch('easygoogle.json')
+    open_mock = mocker.mock_open()
+    mocker.patch('easygoogle.open', open_mock, create=True)
+
+    model_spec = {
+        'refresh_token': 'credentials.refresh_token',
+        'client_id': 'credentials.client_id',
+        'client_secret': 'credentials.client_secret',
+        'token_uri': 'credentials.token_uri',
+        'id_token': 'credentials.id_token',
+        'scopes': ['list(credentials.scopes)'],
+        'token': 'credentials.token'
+    }
+
+    credentials_mock = mocker.MagicMock()
+    credentials_mock.configure_mock(**model_spec)
+    easygoogle.InstalledAppFlow.from_client_secrets_file.return_value = easygoogle.InstalledAppFlow()
+    easygoogle.InstalledAppFlow().run_local_server.return_value = credentials_mock
+
+    oauth = easygoogle.oauth2(mocker.sentinel.json_file, ['scope.unique'],
+                              user="testuser", app_dir=str(tmpdir.dirpath()))
+
+    easygoogle.InstalledAppFlow.from_client_secrets_file.assert_called_once_with(mocker.sentinel.json_file,
+                                                                                 scopes=["https://testscopes.exemple.org/auth/scope.unique"])
+
+    open_mock.assert_called_once_with(str(tmpdir.dirpath('.credentials', 'google_client_library_-_python#testuser.json')), 'w')
+    easygoogle.json.dump.assert_called_once_with(model_spec, open_mock())
+
+    credentials_mock.refresh.assert_called_once_with(easygoogle.google.auth.transport.requests.Request())
+
+    assert oauth.credentials is credentials_mock
