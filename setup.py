0e94bbf4307105ea7ce98f02e6e543d2d06255d8
Luiz Augusto Ferraz <l@mestra.org>
2017-11-05 04:11:47 -0200

    Updated license and requirements on setup file

--- setup.py
+++ setup.py
@@ -4,23 +4,24 @@ from setuptools import setup
 setup(
     name="easygoogle",
     packages=["easygoogle"],
-    license="GNU General Public License v3.0",
-    version='1.0.2',
+    license="Apache License 2.0",
+    version='1.1.0',
     description="Easy to use wrapper to google APIs client library",
     package_data={
         'easygoogle': ['apis.pk']
     },
     author="Luiz Augusto Ferraz",
     author_email="adm.fryuni@gmail.com",
-    install_requires=["google-api-python-client (>=1.6.4)"],
-    url="https://github.com/Fryuni/easy_google",
-    download_url='https://github.com/Fryuni/easy_google/archive/1.0.1.tar.gz',
+    install_requires=["google-api-python-client (~=1.6.4)",
+                      'google-auth (~=1.2.0)'],
+    url="https://github.com/Fryuni/easygoogle",
+    download_url='https://github.com/Fryuni/easygoogle/archive/1.1.0.tar.gz',
     keywords="google apis google-apis",
     classifiers=["Development Status :: 5 - Production/Stable",
                  "Intended Audience :: Developers",
                  "Intended Audience :: Education",
                  "Intended Audience :: Information Technology",
-                 "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                 "License :: OSI Approved :: Apache Software License",
                  "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
                  "Topic :: Utilities"]
 )
