1686948a6cd8afc6a91ec107ba5ade867d751a1a
Luiz Augusto Ferraz <l@mestra.org>
2017-11-21 12:45:13 -0200

    Stricter coverage test

--- easygoogle/__init__.py
+++ easygoogle/__init__.py
@@ -46,14 +46,11 @@ class _api_builder:
             except KeyError:
                 # Log when scope not found
                 logger.warning("[!] SCOPE %s not registered" % x)
-            except Exception as e:
-                # When unexpected error is cought, log and raise RuntimeError
-                logger.error(e)
-                raise RuntimeError from e
             else:
                 # On success log the unlocked APIs
-                logger.debug("Loaded auth: %s --> api: %s" % (
-                    x, ', '.join(("%s, %s" % (a['name'], a['version']) for a in apiset[x]['apis']))))
+                logger.debug("Loaded auth: %s --> api: %s" % (x, ', '.join(
+                    ("%s, %s" % (a['name'], a['version'])
+                     for a in apiset[x]['apis']))))
         logger.info("Apis imported")
 
         # Save the raw apiset
@@ -76,8 +73,11 @@ class _api_builder:
 
         # Build connector if API identifier is valid
         if api in self.valid_apis:
-            res = googleapiclient.discovery.build(self.valid_apis[api][0], self.valid_apis[api][1],
-                                                  credentials=self._credentials, cache_discovery=False)
+            res = googleapiclient.discovery.build(
+                self.valid_apis[api][0],
+                self.valid_apis[api][1],
+                credentials=self._credentials,
+                cache_discovery=False)
             logger.info("%s API Generated" % api)
             return res
         else:
@@ -90,15 +90,22 @@ class _api_builder:
 class oauth2(_api_builder):
 
     # Main constructor function
-    def __init__(self, secret_json, scopes, appname='Google Client Library - Python', user="",
-                 app_dir='.', manualScopes=[], hostname='localhost', port=8080):
+    def __init__(self,
+                 secret_json,
+                 scopes,
+                 appname='Google Client Library - Python',
+                 user="",
+                 app_dir='.',
+                 manualScopes=[],
+                 hostname='localhost',
+                 port=8080):
 
         # Load valid APIs unlocked with the scopes
         self._loadApiNames(scopes)
 
         # Save all scopes results
-        self.SCOPES = list(set([x['scope']
-                                for x in self.apis.values()] + manualScopes))
+        self.SCOPES = list(
+            set([x['scope'] for x in self.apis.values()] + manualScopes))
 
         # Home directory of the app
         home_dir = os.path.abspath(app_dir)
@@ -117,17 +124,20 @@ class oauth2(_api_builder):
             # Construct file name
             self.filename = ''.join(
                 map(chr, (x for x in self.name.encode() if x < 128))).lower()
-            self.filename = self.filename.replace(' ', '_') + '#' + user + ".json"
+            self.filename = self.filename.replace(' ',
+                                                  '_') + '#' + user + ".json"
 
             # Assemble full credential file path
-            self.credential_path = os.path.join(self.credential_dir, self.filename)
+            self.credential_path = os.path.join(self.credential_dir,
+                                                self.filename)
 
             # Load saved credentials if the file exists
             if os.path.isfile(self.credential_path):
                 saved_state = json.load(open(self.credential_path))
                 for s in self.SCOPES:
                     if s not in saved_state['scopes']:
-                        self.SCOPES = list(set(self.SCOPES + saved_state['scopes']))
+                        self.SCOPES = list(
+                            set(self.SCOPES + saved_state['scopes']))
                         saved_state['valid']
                         break
             else:
@@ -136,11 +146,14 @@ class oauth2(_api_builder):
             if saved_state == None:
                 # No valid credentials found
                 # Instantiate authrization flow
-                flow = InstalledAppFlow.from_client_secrets_file(secret_json, scopes=self.SCOPES)
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    secret_json, scopes=self.SCOPES)
 
                 # Start web server to authorize application
                 credentials = flow.run_local_server(host=hostname, port=port)
-                credentials.refresh(google.auth.transport.requests.Request(session=flow.authorized_session()))
+                credentials.refresh(
+                    google.auth.transport.requests.Request(
+                        session=flow.authorized_session()))
 
                 saved_state = {
                     'refresh_token': credentials.refresh_token,
@@ -154,7 +167,8 @@ class oauth2(_api_builder):
 
                 json.dump(saved_state, open(self.credential_path, 'w'))
             else:
-                credentials = google.oauth2.credentials.Credentials(**saved_state)
+                credentials = google.oauth2.credentials.Credentials(
+                    **saved_state)
                 credentials.refresh(google.auth.transport.requests.Request())
             self._credentials = credentials
 
@@ -167,14 +181,20 @@ class oauth2(_api_builder):
 class service_acc(_api_builder):
 
     # Instantiate handler
-    def __init__(self, service_file, scopes, manualScopes=[], domainWide=True, *args, **kwargs):
+    def __init__(self,
+                 service_file,
+                 scopes,
+                 manualScopes=[],
+                 domainWide=True,
+                 *args,
+                 **kwargs):
 
         # Load valid APIs unlocked with the scopes
         self._loadApiNames(scopes)
 
         # Save all scopes results
-        self.SCOPES = list(set([x['scope']
-                                for x in self.apis.values()] + manualScopes))
+        self.SCOPES = list(
+            set([x['scope'] for x in self.apis.values()] + manualScopes))
 
         # Set domain wide delegation flag
         self.__domWide = domainWide
@@ -197,8 +217,8 @@ class service_acc(_api_builder):
         # Proceed only if domain wide delegation flag was setted to True
         if self.__domWide:
             # Instantiate delegated handler
-            res = _delegated(self.credentials.with_subject(
-                user), self.valid_apis)
+            res = _delegated(
+                self.credentials.with_subject(user), self.valid_apis)
             logger.info("Created delegated credentials")
             return res
         else:
--- tests/test_oauth2.py
+++ tests/test_oauth2.py
@@ -8,8 +8,10 @@ __path__ = os.path.dirname(os.path.abspath(__file__))
 MOCKED_APIS = json.load(open(os.path.join(__path__, 'data', 'apis.json')))
 
 SCOPES = ['scope.unique', 'scope.multiple']
-RESULT_SCOPES = ["https://testscopes.exemple.org/auth/scope.multiple",
-                 "https://testscopes.exemple.org/auth/scope.unique"]
+RESULT_SCOPES = [
+    "https://testscopes.exemple.org/auth/scope.multiple",
+    "https://testscopes.exemple.org/auth/scope.unique"
+]
 
 
 def test_inheritance():
@@ -37,18 +39,39 @@ def test_creation_call(mocker, tmpdir):
 
     credentials_mock = mocker.MagicMock()
     credentials_mock.configure_mock(**model_spec)
-    easygoogle.InstalledAppFlow.from_client_secrets_file.return_value = easygoogle.InstalledAppFlow()
-    easygoogle.InstalledAppFlow().run_local_server.return_value = credentials_mock
+    easygoogle.InstalledAppFlow.from_client_secrets_file.return_value = (
+        easygoogle.InstalledAppFlow())
+    easygoogle.InstalledAppFlow().run_local_server.return_value = (
+        credentials_mock)
 
-    oauth = easygoogle.oauth2(mocker.sentinel.json_file, ['scope.unique'],
-                              user="testuser", app_dir=str(tmpdir.dirpath()))
+    oauth = easygoogle.oauth2(
+        mocker.sentinel.json_file, ['scope.unique'],
+        user="testuser",
+        app_dir=str(tmpdir.dirpath()))
 
-    easygoogle.InstalledAppFlow.from_client_secrets_file.assert_called_once_with(mocker.sentinel.json_file,
-                                                                                 scopes=["https://testscopes.exemple.org/auth/scope.unique"])
+    easygoogle.InstalledAppFlow.from_client_secrets_file.assert_called_once_with(
+        mocker.sentinel.json_file,
+        scopes=["https://testscopes.exemple.org/auth/scope.unique"])
 
-    open_mock.assert_called_once_with(str(tmpdir.dirpath('.credentials', 'google_client_library_-_python#testuser.json')), 'w')
+    open_mock.assert_called_once_with(
+        str(
+            tmpdir.dirpath('.credentials',
+                           'google_client_library_-_python#testuser.json')),
+        'w')
     easygoogle.json.dump.assert_called_once_with(model_spec, open_mock())
 
-    credentials_mock.refresh.assert_called_once_with(easygoogle.google.auth.transport.requests.Request())
+    credentials_mock.refresh.assert_called_once_with(
+        easygoogle.google.auth.transport.requests.Request())
 
     assert oauth.credentials is credentials_mock
+
+
+def test_creation_from_default(mocker):
+    mocker.patch.dict('easygoogle.apisDict', values=MOCKED_APIS)
+    mocker.patch('easygoogle.google')
+
+    easygoogle.google.auth.default.return_value = (mocker.sentinel.credentials, mocker.sentinel.project)
+
+    oauth = easygoogle.oauth2(None, ['scope.unique'])
+
+    assert oauth.credentials is mocker.sentinel.credentials
