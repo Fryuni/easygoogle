72037b9b23105e7476d851a0e34d4936685852ce
Luiz Augusto Ferraz <luiz@lferraz.com>
2018-01-26 15:04:55 -0200

    Fixed sintax error

--- easygoogle/__init__.py
+++ easygoogle/__init__.py
@@ -182,9 +182,8 @@ class oauth2(_api_builder):
 
                 json.dump(saved_state, open(self.credential_path, 'w'))
             else:
-                credentials = google.oauth2.credentials.Credentials(
-                    **saved_state,
-                )
+                credentials = google.oauth2 \
+                    .credentials.Credentials(**saved_state)
                 credentials.refresh(
                     google.auth.transport.requests.Request(),
                 )
